2ae7b06a8dcde0647f6c5a3554f3576523287378
hussainitdev <hussainitdev@gmail.com>
2024-03-23 01:41:47 +0500

    demo code
    here is a demo code

---
+++ demo2/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:demo2/view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: '',),
+    );
+  }
+}
